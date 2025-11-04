6caa05ed0b63f67a32b3355ed955cf2019c6e851
achievewithchin <achievewithchinwendu@gmail.com>
2025-11-04 22:55:22 +0100

    Update app.py

--- web/app.py
+++ web/app.py
@@ -4,7 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 CryptoCat Brain Web Service is alive!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return "CryptoCat Intelligence Web Service is running 🧠"
